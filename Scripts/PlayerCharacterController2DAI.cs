951177a4c177e4988e32118299df14dbaa68bfba
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2022-08-01 23:33:59 +0700

    WIP interactable entity improvement (not done yet)

--- Scripts/PlayerCharacterController2DAI.cs
+++ Scripts/PlayerCharacterController2DAI.cs
@@ -103,10 +103,9 @@ namespace MultiplayerARPG
             }
         }
 
-        protected override bool OverlappedEntity<T>(T entity, Vector3 measuringPosition, Vector3 expectedOverlappedPosition, float distance)
+        protected override bool OverlappedEntity(ITargetableEntity entity, Vector3 sourcePosition, Vector3 targetPosition, float distance)
         {
-            // Must reached end of path before doing an actions
-            return base.OverlappedEntity(entity, measuringPosition, expectedOverlappedPosition, distance) && movement.reachedEndOfPath;
+            return base.OverlappedEntity(entity, sourcePosition, targetPosition, distance);
         }
     }
 }
