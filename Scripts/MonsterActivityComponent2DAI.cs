c408434c548c18ef05e99dce1ea9b9bbf87706df
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2024-01-26 02:36:19 +0700

    improve: delete unused codes

--- Scripts/MonsterActivityComponent2DAI.cs
+++ Scripts/MonsterActivityComponent2DAI.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using Pathfinding;
-using UnityEngine.Profiling;
+﻿using UnityEngine;
 
 namespace MultiplayerARPG
 {
