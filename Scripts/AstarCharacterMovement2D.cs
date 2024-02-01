6335fc2eecde35da5dfcc86925b9acad2edb496d
insthync <ittipon.bay@gmail.com>
2024-02-01 22:55:36 +0700

    feat: change Astar movement workflow, now it won't uses `AILerp/AIPath` to move, it will use base RigidbodyEntityMovement2D for movement

--- Scripts/AstarCharacterMovement2D.cs
+++ Scripts/AstarCharacterMovement2D.cs
@@ -6,9 +6,12 @@ namespace MultiplayerARPG
 {
     public class AstarCharacterMovement2D : RigidBodyEntityMovement2D
     {
-        public IAstarAI CacheAIPath { get; private set; }
         public Seeker Seeker { get; private set; }
+
+        protected bool _localReachedEndOfPath = true;
         protected bool _remoteReachedEndOfPath = true;
+        protected float _nodeSize;
+        protected Vector3? _endOfPathPosition;
 
         public bool reachedEndOfPath
         {
@@ -16,7 +19,7 @@ namespace MultiplayerARPG
             {
                 if ((movementSecure == MovementSecure.ServerAuthoritative && IsServer) ||
                     (movementSecure == MovementSecure.NotSecure && IsOwnerClient))
-                    return CacheAIPath.reachedEndOfPath;
+                    return _localReachedEndOfPath;
                 return _remoteReachedEndOfPath;
             }
         }
@@ -24,15 +27,50 @@ namespace MultiplayerARPG
         public override void EntityAwake()
         {
             base.EntityAwake();
-            CacheAIPath = GetComponent<IAstarAI>();
-            if (CacheAIPath == null)
-            {
-                AILerp newPathComp = gameObject.AddComponent<AILerp>();
-                newPathComp.orientation = OrientationMode.YAxisForward;
-                newPathComp.enableRotation = false;
-                CacheAIPath = newPathComp;
-            }
             Seeker = GetComponent<Seeker>();
+            Seeker.startEndModifier.exactStartPoint = StartEndModifier.Exactness.SnapToNode;
+            Seeker.startEndModifier.exactEndPoint = StartEndModifier.Exactness.SnapToNode;
+            Seeker.pathCallback += OnPathComplete;
+        }
+
+        public override void EntityOnDestroy()
+        {
+            base.EntityOnDestroy();
+            Seeker.pathCallback -= OnPathComplete;
+        }
+
+        protected void OnPathComplete(Path _p)
+        {
+            NavPaths = null;
+            NavPaths = new System.Collections.Generic.Queue<Vector2>();
+            GraphNode node;
+            Vector3 nodePosition;
+            for (int i = 0; i < _p.path.Count; ++i)
+            {
+                node = _p.path[i];
+                nodePosition = (Vector3)node.position;
+                NavPaths.Enqueue(nodePosition);
+                if (i == 0 && node.Graph is GridGraph gridGraph)
+                {
+                    _nodeSize = gridGraph.nodeSize;
+                    if (Vector3.Distance(nodePosition, Entity.MovementTransform.position) < _nodeSize)
+                        NavPaths.Dequeue();
+                }
+                _endOfPathPosition = nodePosition;
+            }
+        }
+
+        public override void KeyMovement(Vector3 moveDirection, MovementState movementState)
+        {
+            if (moveDirection.sqrMagnitude <= 0.25f)
+                return;
+            PointClickMovement(CacheTransform.position + moveDirection);
+        }
+
+        public override void PointClickMovement(Vector3 position)
+        {
+            base.PointClickMovement(position);
+            Seeker.StartPath(Entity.MovementTransform.position, position);
         }
 
         public override void OnSetup()
@@ -48,79 +86,14 @@ namespace MultiplayerARPG
 
         public override void EntityUpdate()
         {
-            UpdateMovement(Time.deltaTime);
-
+            base.EntityUpdate();
             // Update reached end of path state
+            _localReachedEndOfPath = true;
+            if (_endOfPathPosition.HasValue && HasNavPaths && Vector3.Distance(_endOfPathPosition.Value, Entity.MovementTransform.position) >= _nodeSize)
+            {
+                _localReachedEndOfPath = false;
+            }
             CallNetFunction(NetFuncSetReachedEndOfPath, 0, LiteNetLib.DeliveryMethod.Sequenced, FunctionReceivers.All, reachedEndOfPath);
-
-            // Force set AILerp settings
-            CacheAIPath.canMove = true;
-            CacheAIPath.canSearch = true;
-            CacheAIPath.maxSpeed = Entity.GetMoveSpeed();
-        }
-
-        public override void KeyMovement(Vector3 moveDirection, MovementState movementState)
-        {
-            if (moveDirection.sqrMagnitude <= 0.25f)
-                return;
-            PointClickMovement(CacheTransform.position + moveDirection);
-        }
-
-        protected override void UpdateMovement(float deltaTime)
-        {
-            if (HasNavPaths && Entity.CanMove())
-            {
-                // Set destination to AI Path
-                CacheAIPath.isStopped = false;
-                Vector3 destination = NavPaths.Peek();
-                if (Vector3.Distance(CacheAIPath.destination, destination) > Mathf.Epsilon)
-                    CacheAIPath.destination = NavPaths.Peek();
-            }
-            else
-            {
-                // Character dead?
-                CacheAIPath.isStopped = true;
-            }
-
-            // Change direction by move direction
-            if (CacheAIPath.velocity.sqrMagnitude > 0.25f)
-                Direction2D = CacheAIPath.velocity.normalized;
-
-            if (IsOwnerClient || (IsServer && movementSecure == MovementSecure.ServerAuthoritative))
-            {
-                // Update movement state
-                MovementState = (CacheAIPath.velocity.sqrMagnitude > 0 ? MovementState.Forward : MovementState.None) | MovementState.IsGrounded;
-                // Update extra movement state
-                ExtraMovementState = this.ValidateExtraMovementState(MovementState, _tempExtraMovementState);
-            }
-
-            // Set inputs
-            if (CacheAIPath.velocity.sqrMagnitude > 0f)
-            {
-                _currentInput = Entity.SetInputMovementState2D(_currentInput, _tempMovementState);
-                _currentInput = Entity.SetInputPosition(_currentInput, CacheAIPath.destination);
-                _currentInput = Entity.SetInputIsKeyMovement(_currentInput, false);
-            }
-            _currentInput = Entity.SetInputDirection2D(_currentInput, Direction2D);
-        }
-
-        public override void SetLookRotation(Quaternion rotation)
-        {
-            if (CacheAIPath.velocity.sqrMagnitude > 0f)
-                return;
-            if (!Entity.CanMove() || !Entity.CanTurn())
-                return;
-            if (CanPredictMovement())
-            {
-                // Always apply movement to owner client (it's client prediction for server auth movement)
-                Direction2D = (Vector2)(rotation * Vector3.forward);
-            }
-        }
-
-        protected override void OnTeleport(Vector2 position, bool stillMoveAfterTeleport)
-        {
-            base.OnTeleport(position, stillMoveAfterTeleport);
-            CacheAIPath.Teleport(position);
         }
     }
 }
